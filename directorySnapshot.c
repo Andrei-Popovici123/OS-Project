3ad6cd2316196858a9a3d82b05ff25479b7767c4
Popovici Andrei-Cristian <andrei.popovici@ligaac.ro>
2024-04-22 15:32:21 +0300

    MAX 10 DIR + snapshot  update

--- directorySnapshot.c
+++ directorySnapshot.c
@@ -9,6 +9,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define MAX_DIRS 10
+
 void monitor_directory(const char *path) {
     struct dirent *pDirent;
     DIR *pDir;
@@ -38,13 +40,58 @@ void monitor_directory(const char *path) {
     closedir(pDir);
 }
 
+void update_snapshot(const char *path) {
+    struct dirent *pDirent;
+    DIR *pDir;
+    char buff[50];
+
+    pDir = opendir(path);
+    if (pDir == NULL) {
+        printf("Cannot open directory '%s'", path);
+        exit(EXIT_FAILURE);
+    }
+    while ((pDirent = readdir(pDir)) != NULL) {
+        char file_path[PATH_MAX];
+        snprintf(file_path, PATH_MAX, "%s/%s", path, pDirent->d_name);
+
+        struct stat file_stat;
+        if (stat(file_path, &file_stat) == 0) {
+            int snapfile = open("snapshot.txt", O_RDWR | O_APPEND);
+            if (snapfile == -1) {
+                printf("Error opening snapshot file");
+                exit(EXIT_FAILURE);
+            }
+            char line[100];
+            char *name;
+            char *mtime_str;
+            while (fgets(line, sizeof(line), snapfile)) {
+                name = strtok(line, "\t");
+                mtime_str = strtok(NULL, "\t");
+                if (name != NULL && mtime_str != NULL) {
+                    if (strcmp(pDirent->d_name, name) == 0) {
+                        long mtime_old = strtol(mtime_str, NULL, 10);
+                        if (file_stat.st_mtime != mtime_old) {
+                            lseek(snapfile, -strlen(line), SEEK_CUR);
+                            snprintf(buff, sizeof(buff), "%s\t%ld\n", pDirent->d_name, file_stat.st_mtime);
+                            write(snapfile, buff, strlen(buff));
+                        }
+                    }
+                }
+            }
+            close(snapfile);
+        }
+    }
+    closedir(pDir);
+}
+
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        perror("Not enough arguments.");
+    if (argc < 2 || argc > MAX_DIRS + 1) {
+        perror("Invalid number of arguments.");
         exit(EXIT_FAILURE);
     }
     for (int i = 1; i < argc; i++) {
         monitor_directory(argv[i]);
+        update_snapshot(argv[i]);
     }
     return 0;
 }
